58136b7097daa7e89c2bcb25e04e21f5fd353160
Jerome Caruso <jerome0caruso@gmail.com>
2021-04-16 17:23:53 -0500

    API project

---
+++ app.js
@@ -0,0 +1,40 @@
+const form = document.getElementById('searchForm');
+const imageContainer = document.querySelector('.images');
+const headingContainer = document.querySelector('.searching');
+const inputTitle = document.querySelector('.inputTitle');
+
+
+form.addEventListener('submit', async function(e) {
+    e.preventDefault();
+    const userInput = form.elements.query.value;
+    const response = await apiCall(userInput);
+    makeImages(response.data, userInput);
+    form.elements.query.value = "";
+})
+
+async function apiCall(userInput) {
+    const config = { params: { q: userInput, isFunny: 'some other query para  m' } }
+    const response = await axios.get('http://api.tvmaze.com/search/shows', config);
+    return response;
+}
+
+const makeImages = (shows, userInput) => {
+    headingContainer.style.visibility = "visible";
+    inputTitle.innerHTML = userInput;
+    deleteImage();
+    for(let result of shows) {
+        if(result.show.image) {
+            const img = document.createElement('IMG');
+            img.src = result.show.image.medium;
+            img.className = "showImg"
+            imageContainer.append(img);
+        }
+    }
+}
+
+function deleteImage() {
+    const deleter = document.querySelectorAll('img');
+    deleter.forEach(image => {
+        image.remove();
+    })
+}
